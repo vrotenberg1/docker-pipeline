08d0847d33e9dac0c166ed74f3b0348cad7eca06
vrotenberg1 <100290291+vrotenberg1@users.noreply.github.com>
2023-01-15 10:38:25 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,9 @@ pipeline {
   stages {
      stage('Build Docker') {
             steps {
+              step{
               def customImage = docker.build("my-image:${env.BUILD_ID}")
+              }
             }
         }
   }
